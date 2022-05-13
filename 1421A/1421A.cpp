9abb39dcdebd78f190b83ac9c4d99757bd0b4465
Lautaro Fernandez Pricco <lpricco@medallia.com>
2022-05-13 16:12:39 -0300

    added input and output examples for 1421A and 959A

--- 1421A/1421A.cpp
+++ 1421A/1421A.cpp
@@ -18,7 +18,14 @@ int solve()
 }
 
 int main()
-{
+{   
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
     int tests, i=0;
     cin >> tests;
     vector<int> results(tests);
--- 959A/959A.cpp
+++ 959A/959A.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int n;
     cin >> n;
 
