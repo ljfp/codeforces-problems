202bd2cadc424554aa988c93b778565365b7e75f
LJFP <ljfp@ljfp.xyz>
2024-10-28 17:53:09 +0000

    Update template.

--- template/main.cpp
+++ template/main.cpp
@@ -5,11 +5,14 @@ using namespace std;
 #define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
 #define REP(i, j) FOR(i, 0, j, 1)
 #define RREP(i, j) RFOR(i, j, 0, 1)
-
-#define INF (int)1e9
 #define PI 3.1415926535897932384626433832795
-#define MOD 1000000007
 
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+typedef long double ld;
 typedef long long ll;
 typedef unsigned long long ull;
 
@@ -28,22 +31,8 @@ typedef map<ll, ll> mll;
 typedef unordered_map<int, int> umap_ii;
 typedef unordered_map<string, int> umap_si;
 
-/**
- * Limits in C++ for reference
- * _____________________________________________________________________________________
- * |Sr| Macro Name | Description                     | Value
- * |No|____________|_________________________________|__________________________________
- * |1.| ULLONG_MAX | Maximum value unsigned long long| 18,446,744,073,709,551,615 (10^20)
- * |2.| LLONG_MAX  | Maximum value long long         | 9,223,372,036,854,775,807 (10^19)
- * |3.| LLONG_MIN  | Minimum value long long         |-9,223,372,036,854,775,808 -1*(10^19)
- * |4.| INT_MAX    | Maximum value int               | 2,147,483,647 (10^10)
- * |5.| INT_MIN    | Minimum value int               |-2,147,483,648 (10^10)
-*/
-
-void solve(ll test_case)
+void solve()
 {
-	// Do your thing here :)
-	cout << test_case << "\n";
 }
 
 int main()
@@ -55,11 +44,10 @@ int main()
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	ll t, t1 = 0;
+	ll t = 0;
 	cin >> t;
-	while (t1 < t)
+	while (t--)
 	{
-		solve(t1 + 1);
-		t1++;
+		solve();
 	}
 }
