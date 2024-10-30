92152072f89c44591d120ed8ef989ed09e0d3120
LJFP <ljfp@ljfp.xyz>
2024-10-30 17:38:58 +0000

    1353B

---
+++ 1353B/1353B.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FOR(i, j, k, in) for (int i = j; i < k; i += in)
+#define RFOR(i, j, k, in) for (int i = j; i >= k; i -= in)
+#define REP(i, j) FOR(i, 0, j, 1)
+#define RREP(i, j) RFOR(i, j, 0, 1)
+#define PI 3.1415926535897932384626433832795
+
+typedef short int si;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned long long ull;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+typedef vector<string> vs;
+typedef vector<int> vi;
+typedef vector<short int> vsi;
+typedef vector<vi> vvi;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<string, string> pss;
+typedef map<int, int> mii;
+typedef map<ll, ll> mll;
+typedef unordered_map<int, int> umap_ii;
+typedef unordered_map<string, int> umap_si;
+
+void solve() {
+	vsi a, b;
+	queue<si> qa, qb;
+	si n, k;
+
+	cin >> n >> k;
+
+	for (si i = 0; i < n; i++) {
+		si x = 0;
+		cin >> x;
+		a.push_back(x);
+	}
+	sort(a.begin(), a.end(), less{});
+
+	for (si i = 0; i < n; i++) {
+		si x = 0;
+		cin >> x;
+		b.push_back(x);
+	}
+	sort(b.begin(), b.end(), greater{});
+
+	int r = 0;
+	for (si i = 0; i < n; i++) {
+		if (i < k && a[i] < b[i]) {
+			a[i] = b[i];
+		}
+		r += a[i];
+	}
+
+	cout << r << "\n";
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	ll t = 0;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+}
